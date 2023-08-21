ddc58bf6945dca0377297d2fb217941393a42795
darshana <darshana@gmail.com>
2023-08-21 19:32:03 +0530

    'Updated'

--- src/main/java/Export.java
+++ src/main/java/Export.java
@@ -47,7 +47,7 @@ public class Export {
             exportOutputStream.close();
             System.out.println("Export  API Response Code :: " + exportConnection.getResponseCode());
 
-            Thread.sleep(100);
+            // Thread.sleep(100);
 
             // Export Status API call to get the download URL
             StringBuilder expStatusResp = new StringBuilder();
