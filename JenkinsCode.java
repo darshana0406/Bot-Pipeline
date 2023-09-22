8d4895c85d537585e718b5e51d67c6d0c1adec09
darshana <darshana@gmail.com>
2023-09-22 23:28:10 +0530

    jar added

--- JenkinsCode.java
+++ JenkinsCode.java
@@ -8,15 +8,22 @@ public class JenkinsCode {
     public static void main(String[] args) {
       
 String REPO_URL = "https://darshana0406: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@github.com/darshana0406/CCT-Bots-Automation.git";
+String WORKSPACE = "C:/ProgramData/Jenkins/.jenkins/workspace/Test"; // Replace with the actual workspace path
+String GIT_TAG = "1.76767"; // Replace with your desired tag name
+
 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
 String TIMESTAMPS = dateFormat.format(new Date());
+
 FileUtils.deleteDirectory(new File(WORKSPACE + "/TMP"));
 FileUtils.forceMkdir(new File(WORKSPACE + "/TMP"));
-Git.cloneRepository()
+
+         Git.cloneRepository()
         .setURI(REPO_URL)
         .setDirectory(new File(WORKSPACE + "/TMP"))
         .call();
+
 FileUtils.copyDirectory(new File(WORKSPACE + "/ExportBot"), new File(WORKSPACE + "/TMP/conv_billing_mgr/dev_nce/Export_All"));
+
 Git git = Git.open(new File(WORKSPACE + "/TMP/conv_billing_mgr/dev_nce/Export_All"));
 StoredConfig config = git.getRepository().getConfig();
 config.setString("credential", null, "helper", "store");
