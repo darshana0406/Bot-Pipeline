5ccef615a8009ebba8682cab2725c2de2876ad0c
darshana <darshana@gmail.com>
2023-09-28 14:27:47 +0530

    updated file

--- src/main/java/GitHubClone.java
+++ src/main/java/GitHubClone.java
@@ -5,21 +5,26 @@ import java.util.Date;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
 import org.eclipse.jgit.transport.RefSpec;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.eclipse.jgit.api.CloneCommand;
 
  
 
 public class GitHubClone {
 	    public static void main(String[] args) throws IOException, GitAPIException {
 
-	String REPO_URL = "https://darshana0406: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@github.com/darshana0406/CCT-Bots-Automation.git";
+	String REPO_URL = "https://darshana0406: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@github.com/darshana0406/CCT-Bots-Automation.git";
 	String username = "darshana0406";
-	String password = "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";
+	// String password = "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";
+	String password = "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";
 	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
 	String WORKSPACE = "C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\Test";
 	String GIT_TAG = "cct_ivr_billing" ;
 	String TIMESTAMPS = dateFormat.format(new Date());
+	
 	String exportType = "Export_Tasks";
     String env = "prod";
         
@@ -36,10 +41,21 @@ public class GitHubClone {
 
 	FileUtils.deleteDirectory(new File(WORKSPACE + "/TMP"));
 	FileUtils.forceMkdir(new File(WORKSPACE + "/TMP"));
+
+		// 	// Create a new Git repository object.
+        // Repository repository = new RepositoryBuilder().build();
+
+		// 	 // Filter the contents of the local repository to only include the desired folder.
+		//  repository.getRefDatabase().updateRef("HEAD", "refs/heads/master^{tree}");
+		//  repository.getConfig().setString("core.sparsecheckout", "true");
+		//  repository.getConfig().setString("core.sparsecheckout", "/path/to/desired/folder");
+		//  repository.getConfig().save();	
+
 	Git git = Git.cloneRepository()
 	        .setURI(REPO_URL)
 	        .setDirectory(new File(WORKSPACE + "/TMP"))
 	        .call();
+
 	FileUtils.copyDirectory(new File(WORKSPACE + "/ExportBot"),new File(WORKSPACE +
 			"/TMP/cct_ivr_billing/" + env +"_nce/" + exportType + "/ExportBot"));
 	FileUtils.copyFile(new File(WORKSPACE+"\\fullexport.zip"), new File(WORKSPACE +
@@ -61,5 +77,6 @@ public class GitHubClone {
 	System.out.println("Files are pushed to main branch of target repo.");
 
 	// FileUtils.deleteDirectory(new File(WORKSPACE + "/TMP"));
+	// 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
 	        }
 	    }
