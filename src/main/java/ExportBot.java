f84be2ba8b1695e0329eca783fb0fe1202e5160a
darshana <darshana@gmail.com>
2023-09-29 02:24:54 +0530

    'Updated'

--- src/main/java/ExportBot.java
+++ src/main/java/ExportBot.java
@@ -31,7 +31,7 @@ public class ExportBot {
 
               HttpURLConnection exportStatusConnection = null;
 
-              String exportType = "ExportNLP";
+              String exportType = "ExportBotTasks";
               String env = "qa";
               String botName = "cct_ivr_billing";
         
