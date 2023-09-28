05f116ec94a7d6a6ec3be0dd25115b68018c593a
darshana <darshana@gmail.com>
2023-09-29 02:20:37 +0530

    'Updated'

--- src/main/java/ExportEVariable.java
+++ src/main/java/ExportEVariable.java
@@ -1,6 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ExportEVariable {
-    public static void setEnvironmentVariables(String botName, String env, String exportType ) {    
-       
+
+    private static final Map<String, String> botDtlMap = new HashMap<>();
+    static {
+    	botDtlMap.put("cct_ivr_billing-devBotId", "botId");
+    	botDtlMap.put("cct_ivr_billing-qaBotId", "st-c72008d0-ce30-571b-ba13-1078b89dbef4");
+    	botDtlMap.put("cct_ivr_billing-prodBotId", "st-fa3c2d6e-128d-5e18-a60a-eca34e4a9132");
+    	botDtlMap.put("cct_ivr_billing-devJwtToken", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLTUwZDA1N2YwLWY2MDUtNTljYS05OGFjLWI5MWJhMGQ3MmVlMiJ9.xmrKV3z7a9yoDlG7geOlCbaxgzWzVvvXM40LhdJya1A");
+    	botDtlMap.put("cct_ivr_billing-qaJwtToken", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLWJmNWFkNGUyLTE4MjgtNTc2MS04YmY2LTQyNjg2OGI0NWUyYiJ9.tl5Cy379FH0Tws4Mu2f9uDSnKoh0JIaxFgD5XjgQD3k");
+    	botDtlMap.put("cct_ivr_billing-prodJwtToken", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLWI2ZDY4Njk3LTA1ZmEtNTQwNC1iNzg4LTIxNWE3MWUwMjc0OSJ9.bRkzPwrHF2aWLhvS3e6iEI72XVsk6nuUVPWl-z0VaFQ");
+    }
+    public static void setEnvironmentVariables(String botName, String env, String exportType ) { 
+        String botIdKey = botName + "-" + env + "BotId";
+        String botId = "";
+        if (botDtlMap.containsKey(botIdKey)) {
+            botId = botDtlMap.get(botIdKey);
+        }
+        String JwtToken ="";
+        String JwtKey = botName+"-"+env+"JwtToken";
+        if (botDtlMap.containsKey(JwtKey)) {
+            JwtToken = botDtlMap.get(JwtKey);
+        }
+       System.out.print(JwtToken);
+       System.out.print(botId);
         //condition for Export only NLPData
         if(exportType.equals("ExportNLP")){
             System.setProperty("Export_Body","{\"exportType\": \"published\",\"exportOptions\": {\"nlpData\": [\"training_data\",\"bot_synonyms\",\"defaultDialog\",\"nlpSettings\",\"utterances\",\"patterns\",\"standardResponses\"]},\"allTasks\": true,\"IncludeDependentTasks\": true}");
@@ -32,23 +56,23 @@ public class ExportEVariable {
 
         // for prod use workspace : VB and bot : demobot1
         if(env.equals("prod")){
-            System.setProperty("Export_JWT", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLWI2ZDY4Njk3LTA1ZmEtNTQwNC1iNzg4LTIxNWE3MWUwMjc0OSJ9.bRkzPwrHF2aWLhvS3e6iEI72XVsk6nuUVPWl-z0VaFQ");
-            System.setProperty("Export_URL", "https://bots.kore.ai/api/public/bot/st-fa3c2d6e-128d-5e18-a60a-eca34e4a9132/export");
-            System.setProperty("ExportStatus_URL", "https://bots.kore.ai/api/public/bot/st-fa3c2d6e-128d-5e18-a60a-eca34e4a9132/export/status");
+            System.setProperty("Export_JWT", JwtToken);
+            System.setProperty("Export_URL", "https://bots.kore.ai/api/public/bot/" +botId +"/export");
+            System.setProperty("ExportStatus_URL", "https://bots.kore.ai/api/public/bot/" +botId +"/export/status");
         
         }
         // for qa use workspace : VB and bot : demobot2
         else if(env.equals("qa")){
-            System.setProperty("Export_JWT", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLWJmNWFkNGUyLTE4MjgtNTc2MS04YmY2LTQyNjg2OGI0NWUyYiJ9.tl5Cy379FH0Tws4Mu2f9uDSnKoh0JIaxFgD5XjgQD3k");
-            System.setProperty("Export_URL", "https://bots.kore.ai/api/public/bot/st-c72008d0-ce30-571b-ba13-1078b89dbef4/export");
-            System.setProperty("ExportStatus_URL", "https://bots.kore.ai/api/public/bot/st-c72008d0-ce30-571b-ba13-1078b89dbef4/export/status");
+            System.setProperty("Export_JWT",JwtToken);
+            System.setProperty("Export_URL", "https://bots.kore.ai/api/public/bot/" +botId +"/export");
+            System.setProperty("ExportStatus_URL", "https://bots.kore.ai/api/public/bot/" +botId +"/export/status");
             
         }
         // for dev use diffrent account workspace : DB and bot : Exportbot
         else {
-            System.setProperty("Export_JWT", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLTUwZDA1N2YwLWY2MDUtNTljYS05OGFjLWI5MWJhMGQ3MmVlMiJ9.xmrKV3z7a9yoDlG7geOlCbaxgzWzVvvXM40LhdJya1A");
-            System.setProperty("Export_URL", "https://bots.kore.ai/api/public/bot/st-e5669197-991d-5971-9417-a422368a0805/export");
-            System.setProperty("ExportStatus_URL", "https://bots.kore.ai/api/public/bot/st-e5669197-991d-5971-9417-a422368a0805/export/status");
+            System.setProperty("Export_JWT", JwtToken);
+            System.setProperty("Export_URL", "https://bots.kore.ai/api/public/bot/" +botId +"/export");
+            System.setProperty("ExportStatus_URL", "https://bots.kore.ai/api/public/bot/" +botId +"/export/status");
           
         }
             
