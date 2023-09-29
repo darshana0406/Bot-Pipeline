0c9c6f48fc2f89c0da80f55dd15cbd50481db9f9
darshana <darshana@gmail.com>
2023-09-29 22:08:57 +0530

    'Updated'

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -23,7 +23,7 @@ public class UI {
     
    public static void main(String[] args) throws Exception {
 
-        String tagName = "cct_ivr_billing-qa-ExportAll-20230929215056";
+        String tagName = "cct_ivr_billing-qa-ExportAll-20230929220312";
         
         FileUtils.deleteDirectory(new File("c:\\Users\\gg\\Documents\\GITTags"));
 
