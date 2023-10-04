a6b76a31af32b3631e72ba4d6f28141aa51f04c5
Darshana V <db@gmail.com>
2023-10-04 12:17:06 +0530

    'Updated'

--- src/main/java/ExportBot.java
+++ src/main/java/ExportBot.java
@@ -309,7 +309,7 @@ public class ExportBot {
 					}
 				}
 			}
-			git.rm().addFilepattern(".").call();
+			git.rm().addFilepattern("cct_ivr_billing/.").call();
 			FileUtils.copyDirectory(new File(workspace + "/ExportBot"), new File(
 					workspace + BotConstants.TMP_PATH + "/" + botName + "/" + env + "/" + exportType + "/ExportBot"));
 
